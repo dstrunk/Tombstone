c0e76c17382007bef65ad18e89d71378920d1a96
Daniel Strunk <me@dstrunk.com>
2015-02-22 20:22:57 -0500

    add mailers, views for aura states

--- app/mailers/aura_mailer.rb
+++ app/mailers/aura_mailer.rb
@@ -10,4 +10,28 @@ class AuraMailer < ActionMailer::Base
     @aura = aura
     mail(to: @user.email, subject: "Your aura request has been submitted")
   end
+
+  def aura_approved(user, aura)
+    @user = user
+    @aura = aura
+    mail(to: @user.email, subject: "Your aura request has been approved")
+  end
+
+  def aura_rejected(user, aura)
+    @user = user
+    @aura = aura
+    mail(to: @user.email, subject: "Your aura request was rejected")
+  end
+
+  def aura_live(user, aura)
+    @user = user
+    @aura = aura
+    mail(to: @user.email, subject: "Your aura request was just released!")
+  end
+
+  def aura_archived(user, aura)
+    @user = user
+    @aura = aura
+    mail(to: @user.email, subject: "Your aura has been archived")
+  end
 end
