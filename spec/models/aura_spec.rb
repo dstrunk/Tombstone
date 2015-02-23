7aeb2cea03cd38b176df8c8bf74111c101926a14
Daniel Strunk <me@dstrunk.com>
2015-02-22 22:54:20 -0500

    finish transition specs

--- spec/models/aura_spec.rb
+++ spec/models/aura_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Aura, :type => :model do
   it { should validate_presence_of(:end_date) }
   it { should validate_uniqueness_of(:name) }
 
-  describe 'transitions' do
+  describe 'unstarted transitions' do
     let(:aura) { FactoryGirl.create(:aura) }
     it 'is in state unstarted by default' do
       expect(aura.current_state).to eq 'unstarted'
@@ -32,4 +32,19 @@ RSpec.describe Aura, :type => :model do
     end
   end
 
+  describe 'accepted and rejected transitions' do
+
+    let(:accepted_aura) { FactoryGirl.create(:aura, :accepted) }
+    let(:rejected_aura) { FactoryGirl.create(:aura, :rejected) }
+
+    it 'accepted transitions can only transition to live' do
+      expect { accepted_aura.transition_to!(:archived) }.to raise_error(Statesman::TransitionFailedError)
+      expect { accepted_aura.transition_to!(:live) }.to_not raise_error
+    end
+
+    it 'rejected transitions can only transition to archived' do
+      expect { rejected_aura.transition_to!(:live) }.to raise_error(Statesman::TransitionFailedError)
+      expect { rejected_aura.transition_to!(:archived) }.to_not raise_error
+    end
+  end
 end
