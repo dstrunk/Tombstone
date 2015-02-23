483b7fdaa7e9a04307cd70cce6e05960a52c8e62
Daniel Strunk <me@dstrunk.com>
2015-02-23 16:30:19 -0500

    misspelled false?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = fasle
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
   # Mandrill settings
   config.action_mailer.smtp_settings = {
