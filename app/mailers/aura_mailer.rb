18d28dd6815b42db000582e1ff875d65d7586ff8
Daniel Strunk <me@dstrunk.com>
2015-02-24 15:52:58 -0500

    make admin array an instance varialbe

--- app/mailers/aura_mailer.rb
+++ app/mailers/aura_mailer.rb
@@ -1,6 +1,6 @@
 class AuraMailer < ActionMailer::Base
-  admin_array = []
-  admins = User.where(admin: true).each { |u| admin_array.push(u.email) }
+  @admin_array = []
+  admins = User.where(admin: true).each { |u| @admin_array.push(u.email) }
 
   default from: "auras@communicorp.com", return_path: admin_array
 
@@ -8,34 +8,34 @@ class AuraMailer < ActionMailer::Base
     @user = user
     @aura = aura
     attachments.inline['tombstone-email-logo.gif'] = File.read("#{Rails.root}/app/assets/images/tombstone-email-logo.gif")
-    mail(to: @user.email, cc: admin_array, subject: "Your aura request has been submitted - #{@aura.job_num}")
+    mail(to: @user.email, cc: @admin_array, subject: "Your aura request has been submitted - #{@aura.job_num}")
   end
 
   def aura_approved(user, aura)
     @user = user
     @aura = aura
     attachments.inline['tombstone-email-logo.gif'] = File.read("#{Rails.root}/app/assets/images/tombstone-email-logo.gif")
-    mail(to: @user.email, cc: admin_array, subject: "Your aura request has been approved - #{@aura.job_num}")
+    mail(to: @user.email, cc: @admin_array, subject: "Your aura request has been approved - #{@aura.job_num}")
   end
 
   def aura_rejected(user, aura)
     @user = user
     @aura = aura
     attachments.inline['tombstone-email-logo.gif'] = File.read("#{Rails.root}/app/assets/images/tombstone-email-logo.gif")
-    mail(to: @user.email, cc: admin_array, subject: "Your aura request was rejected - #{@aura.job_num}")
+    mail(to: @user.email, cc: @admin_array, subject: "Your aura request was rejected - #{@aura.job_num}")
   end
 
   def aura_live(user, aura)
     @user = user
     @aura = aura
     attachments.inline['tombstone-email-logo.gif'] = File.read("#{Rails.root}/app/assets/images/tombstone-email-logo.gif")
-    mail(to: @user.email, cc: admin_array, subject: "Your aura request was just released! - #{@aura.job_num}")
+    mail(to: @user.email, cc: @admin_array, subject: "Your aura request was just released! - #{@aura.job_num}")
   end
 
   def aura_archived(user, aura)
     @user = user
     @aura = aura
     attachments.inline['tombstone-email-logo.gif'] = File.read("#{Rails.root}/app/assets/images/tombstone-email-logo.gif")
-    mail(to: @user.email, cc: admin_array, subject: "Your aura has been archived - #{@aura.job_num}")
+    mail(to: @user.email, cc: @admin_array, subject: "Your aura has been archived - #{@aura.job_num}")
   end
 end
