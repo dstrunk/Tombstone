72f8ee7d3499be5c0b9460082deb802f544cd4d6
Daniel Strunk <me@dstrunk.com>
2015-03-09 10:31:34 -0400

    remove configure_server from redis config

--- config/initializers/sidekiq_configuration.rb
+++ config/initializers/sidekiq_configuration.rb
@@ -3,7 +3,3 @@ require 'redis'
 Sidekiq.configure_server do |config|
   config.redis { namespace: "tombstone" }
 end
-
-Sidekiq.configure_client do |config|
-  config.redis { namespace: "tombstone" }
-end
